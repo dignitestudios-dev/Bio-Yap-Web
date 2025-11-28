4f8d2dd2f83e627566711e80d7727b06eb18517d
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2025-11-28 02:48:08 -0800

    Update:add text

--- src/pages/app/Home.jsx
+++ src/pages/app/Home.jsx
@@ -218,7 +218,7 @@ const Home = () => {
                   : ""
               }`}
             >
-              {withdrawLoading ? "Processing..." : "Review Withdraw"}
+              {withdrawLoading ? "Processing..." : "Test Withdraw"}
             </button>
           </>
         )}
